88ce6be5e0e2e7fb622d3240b9dffaf7598087cf
abychoi <abych8@gmail.com>
2024-09-05 15:30:28 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clínica Médica Salud y Armonía</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <img src="img/logo.jpeg" alt="Logo de la Clínica Médica">
+        <h1>Clínica Médica Salud y Armonía</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="sobre nosotros.php">Sobre Nosotros</a></li>
+                <li><a href="servicios.php">Servicios</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="contacto.php">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h2>Bienvenido a la Clínica Médica Salud y Armonía</h2>
+        <p>En nuestra clínica ofrecemos atención médica de calidad con un equipo de profesionales altamente capacitados. Nos especializamos en ofrecer servicios médicos integrales con un enfoque en la prevención y el bienestar general.</p>
+
+        <section id="mision-vision">
+            <h3>Misión y Visión</h3>
+            <p><strong>Misión:</strong> Proveer servicios médicos de alta calidad con un enfoque en la atención integral, la prevención de enfermedades y el bienestar general de nuestros pacientes.</p>
+            <p><strong>Visión:</strong> Ser una clínica de referencia en el ámbito de la salud, reconocida por la excelencia en la atención médica y el compromiso con la comunidad.</p>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2024 Clínica Médica Salud y Armonía. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
